cd267e043044e3c24f48b84abea3b42d5cf44c88
fruit-in <42092501+fruit-in@users.noreply.github.com>
2020-12-07 04:44:12 +0800

    + github hosts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -289,6 +289,10 @@
 
   networking = { # {{{
     extraHosts = ''
+      151.101.184.133 assets-cdn.github.com
+      151.101.184.133 gist.githubusercontent.com
+      151.101.184.133 cloud.githubusercontent.com
+      151.101.184.133 camo.githubusercontent.com
       151.101.184.133 raw.githubusercontent.com
       151.101.184.133 avatars0.githubusercontent.com
       151.101.184.133 avatars1.githubusercontent.com
