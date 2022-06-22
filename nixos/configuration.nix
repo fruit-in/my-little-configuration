2e701ad64c54ff96db93ec9adfbdb402f1b2cb61
fruit-in <42092501+fruit-in@users.noreply.github.com>
2022-06-22 22:30:53 +0800

    github hosts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -264,22 +264,23 @@
 
   networking = { # {{{
     extraHosts = ''
-      151.101.184.133 assets-cdn.github.com
-      151.101.184.133 gist.githubusercontent.com
-      151.101.184.133 cloud.githubusercontent.com
-      151.101.184.133 camo.githubusercontent.com
-      151.101.184.133 raw.githubusercontent.com
-      151.101.184.133 user-images.githubusercontent.com
-      151.101.184.133 avatars.githubusercontent.com
-      151.101.184.133 avatars0.githubusercontent.com
-      151.101.184.133 avatars1.githubusercontent.com
-      151.101.184.133 avatars2.githubusercontent.com
-      151.101.184.133 avatars3.githubusercontent.com
-      151.101.184.133 avatars4.githubusercontent.com
-      151.101.184.133 avatars5.githubusercontent.com
-      151.101.184.133 avatars6.githubusercontent.com
-      151.101.184.133 avatars7.githubusercontent.com
-      151.101.184.133 avatars8.githubusercontent.com
+      140.82.114.3 github.com
+      185.199.108.153 assets-cdn.github.com
+      185.199.111.133 gist.githubusercontent.com
+      185.199.111.133 cloud.githubusercontent.com
+      185.199.111.133 camo.githubusercontent.com
+      185.199.111.133 raw.githubusercontent.com
+      185.199.111.133 user-images.githubusercontent.com
+      185.199.111.133 avatars.githubusercontent.com
+      185.199.111.133 avatars0.githubusercontent.com
+      185.199.111.133 avatars1.githubusercontent.com
+      185.199.111.133 avatars2.githubusercontent.com
+      185.199.111.133 avatars3.githubusercontent.com
+      185.199.111.133 avatars4.githubusercontent.com
+      185.199.111.133 avatars5.githubusercontent.com
+      185.199.111.133 avatars6.githubusercontent.com
+      185.199.111.133 avatars7.githubusercontent.com
+      185.199.111.133 avatars8.githubusercontent.com
     '';
     firewall.allowedTCPPorts = [ 20427 ];
     hostName = "in";
